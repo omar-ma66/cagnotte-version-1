0c6f240e68039955b1ff0d8c12ffc43170639308
omar-ma66 <devcodefree66@gmail.com>
2026-06-11 14:28:29 +0200

    mise en place du systeme de paiement

--- src/Controller/PaiementController.php
+++ src/Controller/PaiementController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Controller;
-
+use Stripe\Stripe;
+use Stripe\Checkout\Session;
 use App\Entity\Campagne;
 use App\Form\CampagneType;
 use App\Entity\Paiement;
@@ -14,10 +15,64 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route('/paiement')]
 final class PaiementController extends AbstractController
-{
+{   
+
+// ################################################################################################################################################
+
+   #[route('/paiement/produit/test',name:'app_payment_produit_test')] 
+   public function paiement_test_start():Response
+   {
+    return $this->render("paiement/paiement_test.html.twig");
+   }
+   #[Route('/paiement/test', name: 'app_paiement_test')]
+    public function checkout(): Response
+    {
+        // 1. Initialiser Stripe avec la clé secrète stockée dans les paramètres
+        Stripe::setApiKey($this->getParameter('stripe_secret_key'));
+
+        // 2. Créer la session Stripe Checkout
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'eur',
+                    'product_data' => [
+                        'name' => 'Don pour la campagne collaborative',
+                    ],
+                    'unit_amount' => 2000, // Prix en centimes (ici 20,00 €)
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            // Génération des URLs absolues pour le retour sur votre site
+            'success_url' => $this->generateUrl('app_paiement_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('app_paiement_cancel', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+
+        // 3. Rediriger l'utilisateur vers la page hébergée par Stripe
+        return $this->redirect($session->url, 303);
+    }
+
+
+    #[Route('/paiement/test/success',name:'app_paiement_success')]
+    public function paiement_success():Response
+    {
+
+            return new Response(" Stripe OK ca marche");
+    }
+    
+    #[Route('/paiement/test/cancel',name:'app_paiement_cancel')]
+    public function paiement_cancel():Response
+    {
+
+            return new Response("Stripe Echec");
+    }
+
+// ################################################################################################################################################
     #[Route(name: 'app_paiement_index', methods: ['GET'])]
     public function index(PaiementRepository $paiementRepository): Response
     {
@@ -25,6 +80,7 @@ final class PaiementController extends AbstractController
             'paiements' => $paiementRepository->findAll(),
         ]);
     }
+// ################################################################################################################################################
 
     #[Route('/new', name: 'app_paiement_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager,CampagneRepository $campagneRepository): Response
@@ -71,6 +127,7 @@ final class PaiementController extends AbstractController
         ]);
         
     }
+// ################################################################################################################################################
 
     
     // #[Route('/new/{id}', name: 'app_paiement_new', methods: ['GET', 'POST'])]
@@ -104,6 +161,7 @@ final class PaiementController extends AbstractController
     //         'form' => $form,
     //     ]);
     // }
+// ################################################################################################################################################
 
     #[Route('/{id}', name: 'app_paiement_show', methods: ['GET'])]
     public function show(Paiement $paiement): Response
@@ -112,6 +170,7 @@ final class PaiementController extends AbstractController
             'paiement' => $paiement,
         ]);
     }
+// ################################################################################################################################################
 
     #[Route('/{id}/edit', name: 'app_paiement_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Paiement $paiement, EntityManagerInterface $entityManager): Response
@@ -130,6 +189,7 @@ final class PaiementController extends AbstractController
             'form' => $form,
         ]);
     }
+// ################################################################################################################################################
 
     #[Route('/{id}', name: 'app_paiement_delete', methods: ['POST'])]
     public function delete(Request $request, Paiement $paiement, EntityManagerInterface $entityManager): Response
@@ -142,3 +202,5 @@ final class PaiementController extends AbstractController
         return $this->redirectToRoute('app_paiement_index', [], Response::HTTP_SEE_OTHER);
     }
 }
+// ################################################################################################################################################
+
